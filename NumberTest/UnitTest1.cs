c001607f107724cc1553eca429f0c1d6e4d312b8
Rahul Kumar <rahul.kumar@ajmerainfotech.com>
2023-08-28 15:35:16 +0530

    Change

--- NumberTest/UnitTest1.cs
+++ NumberTest/UnitTest1.cs
@@ -8,12 +8,9 @@ namespace NumberTest
         }
 
         [TestCaseSource(nameof(SourceProvider))]
-        public void WhenDivideTwoNumbers_ThenReturnDivisionOfTwoNumbers()
+        public void WhenDivideTwoNumbers_ThenReturnDivisionOfTwoNumbers(int a, int b, int expected)
         {
-            // Arrange
-            var a = 300;
-            var b = 100;
-            var expected = 3;
+
             // Act
             var actual = a / b;
             // Assert
@@ -22,17 +19,18 @@ namespace NumberTest
 
         public static IEnumerable<int[]> SourceProvider()
         {
+            // Arrange
             yield return new int[] { 300, 100, 3 };
             yield return new int[] { 400, 200, 2 };
             yield return new int[] { 100, 100, 1 };
             yield return new int[] { 400, 100, 4 };
-            yield return new int[] { 300, 50, 4 };
+            yield return new int[] { 300, 50, 6 };
             yield return new int[] { 400, 20, 20 };
             yield return new int[] { 30, 10, 3 };
             yield return new int[] { 500, 100, 5 };
             yield return new int[] { 300, 100, 3 };
             yield return new int[] { 400, 200, 1 };
-            yield return new int[] { 300, 100, 7 };
+            yield return new int[] { 300, 100, 3 };
             yield return new int[] { 400, 200, 8 };
         }
     }
