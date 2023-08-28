86fa576968a1ea3696a66a3750cce2a141ebdf22
Rahul Kumar <rahul.kumar@ajmerainfotech.com>
2023-08-28 15:29:57 +0530

    Test

--- NumberTest/UnitTest1.cs
+++ NumberTest/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace NumberTest
         {
         }
 
-        [Test]
+        [TestCaseSource(nameof(SourceProvider))]
         public void WhenDivideTwoNumbers_ThenReturnDivisionOfTwoNumbers()
         {
             // Arrange
@@ -18,6 +18,22 @@ namespace NumberTest
             var actual = a / b;
             // Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<int[]> SourceProvider()
+        {
+            yield return new int[] { 300, 100, 3 };
+            yield return new int[] { 400, 200, 2 };
+            yield return new int[] { 100, 100, 1 };
+            yield return new int[] { 400, 100, 4 };
+            yield return new int[] { 300, 50, 4 };
+            yield return new int[] { 400, 20, 20 };
+            yield return new int[] { 30, 10, 3 };
+            yield return new int[] { 500, 100, 5 };
+            yield return new int[] { 300, 100, 3 };
+            yield return new int[] { 400, 200, 1 };
+            yield return new int[] { 300, 100, 7 };
+            yield return new int[] { 400, 200, 8 };
         }
     }
 }
